edebbff30a61cf2b0c1eb96eb4d230aca09d2715
ginkgo <alexvid33@gmail.com>
2022-03-17 09:50:22 +0100

    add JSON cards?

--- JS/addCards.js
+++ JS/addCards.js
@@ -1,22 +1,24 @@
-const requestURL = "../JSON/index.json";
+const requestURL = "../JSON/agenda.json";
 
 async function fetchAnimalsJSON(){
     const response = await fetch(requestURL);
-    const addCards = await response.json();
-    return addCards;
+    const agenda = await response.json();
+    return agenda;
 };
 
-fetchAnimalsJSON().then(addCards => {
-    for (let index = 0; index < addCards.animals.length; index++) {
-        let pet_name = addCards.animals[index].pet_name;
-        let doctor = addCards.animals[index].doctor;
-        let date = addCards.animals[index].date;
+fetchAnimalsJSON().then(agenda => {
+    for (let index = 0; index < agenda.animals.length; index++) {
+        let pet_name = agenda.animals[index].pet_name;
+        let doctor = agenda.animals[index].doctor;
+        let date = agenda.animals[index].date;
         cards.innerHTML += `
-        <div>
-            <p>${pet_name}</p>
-            <p>${doctor}</p>
-            <p>${date}</p>
-        </div>
+        <section>
+            <div>
+                <p>${pet_name}</p>
+                <p>${doctor}</p>
+                <p>${date}</p>
+            </div>
+        </section>
         `
         }
 })
