25f0cdb7bfcee8f66fd527c68d88494405d19fe0
ginkgo <alexvid33@gmail.com>
2022-03-17 10:29:03 +0100

    Correctly fixed CRUD with Postman

--- JS/addCards.js
+++ JS/addCards.js
@@ -1,22 +1,25 @@
+//console.log('TEST');
+
 const requestURL = "../JSON/agenda.json";
 
-async function fetchAnimalsJSON(){
+async function fetchAgendaJSON(){
     const response = await fetch(requestURL);
     const agenda = await response.json();
     return agenda;
 };
 
-fetchAnimalsJSON().then(agenda => {
+fetchAgendaJSON().then(agenda => {
+    console.log(agenda);
     for (let index = 0; index < agenda.animals.length; index++) {
         let pet_name = agenda.animals[index].pet_name;
         let doctor = agenda.animals[index].doctor;
         let date = agenda.animals[index].date;
         cards.innerHTML += `
-        <section>
+        <section class="box-content h-15 w-80% p-4 border-4 from-slate-200 bg-teal-100">
             <div>
-                <p>${pet_name}</p>
-                <p>${doctor}</p>
-                <p>${date}</p>
+                <p>Pet Name: ${pet_name}</p>
+                <p>Dr. :${doctor}</p>
+                <p>Date: ${date}</p>
             </div>
         </section>
         `
